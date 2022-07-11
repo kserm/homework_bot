1a55f294c5f85a09e6325e053111b450ef0ab3ce
Konstantin Ermachkov <kermachkov@gmail.com>
2022-07-11 19:07:25 +0300

    review fixes #1

--- homework.py
+++ homework.py
@@ -6,6 +6,7 @@ import logging
 import time
 import telegram
 import requests
+import settings
 from http import HTTPStatus
 from dotenv import load_dotenv
 from logging import StreamHandler
@@ -15,17 +16,6 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
 
 
 logger = logging.getLogger(__name__)
@@ -61,15 +51,15 @@ def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     try:
-        homework_statuses = requests.get(ENDPOINT,
-                                         headers=HEADERS,
+        homework_statuses = requests.get(settings.ENDPOINT,
+                                         headers=settings.HEADERS,
                                          params=params)
         if homework_statuses.status_code == HTTPStatus.OK:
             return homework_statuses.json()
         elif homework_statuses.status_code == HTTPStatus.NOT_FOUND:
             error_logging(
                 'Сбой в работе программы: '
-                f'Эндпоинт {ENDPOINT} недоступен. '
+                f'Эндпоинт {settings.ENDPOINT} недоступен. '
                 f'Код ответа API: {homework_statuses.status_code}'
             )
         else:
@@ -86,17 +76,15 @@ def check_response(response):
         error_logging('Ответ API содержит пустой словарь')
         raise IndexError('Ответ API содержит пустой словарь')
     elif type(response) == dict:
-        expected_keys = ['current_date', 'homeworks']
-        for key in response:
-            if key in expected_keys:
-                homeworks = response['homeworks']
-                if isinstance(homeworks, list):
-                    return homeworks
-                else:
-                    raise TypeError('Объект homeworks не является списком')
+        if settings.HOMEWORKS_KEY in response:
+            homeworks = response['homeworks']
+            if isinstance(homeworks, list):
+                return homeworks
             else:
-                error_logging(f'В ответе API отсутствует ключ {key}')
-                raise KeyError(f'В ответе API отсутствует ключ {key}')
+                raise TypeError('Объект homeworks не является списком')
+        else:
+            error_logging(f'В ответе API отсутствует ключ {key}')
+            raise KeyError(f'В ответе API отсутствует ключ {key}')
     else:
         error_logging('Ответ API не приведен к типам данных Python')
         raise TypeError('Ответ API не приведен к типам данных Python')
@@ -104,10 +92,18 @@ def check_response(response):
 
 def parse_status(homework):
     """Parse homework status from response data."""
-    homework_name = homework['homework_name']
-    homework_status = homework['status']
-    if homework_status in HOMEWORK_STATUSES.keys():
-        verdict = HOMEWORK_STATUSES[homework_status]
+    if settings.HOMEWORK_NAME_KEY in homework:
+        homework_name = homework[settings.HOMEWORK_NAME_KEY]
+    else:
+        error_logging(f'Отсутствует ключ {settings.HOMEWORK_NAME_KEY}')
+        raise KeyError(f'Отсутствует ключ {settings.HOMEWORK_NAME_KEY}')
+    if settings.HOMEWORK_STATUS_KEY in homework:
+        homework_status = homework[settings.HOMEWORK_STATUS_KEY]
+    else:
+        error_logging(f'Отсутствует ключ {settings.HOMEWORK_STATUS_KEY}')
+        raise KeyError(f'Отсутствует ключ {settings.HOMEWORK_STATUS_KEY}')
+    if homework_status in settings.HOMEWORK_STATUSES:
+        verdict = settings.HOMEWORK_STATUSES[homework_status]
         return f'Изменился статус проверки работы "{homework_name}". {verdict}'
     else:
         error_logging('Обнаружен недокументированный статус'
@@ -136,29 +132,28 @@ def check_tokens():
 
 def main():
     """Основная логика работы бота."""
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-    check_tokens()
-    while True:
-        try:
-            response = get_api_answer(current_timestamp)
-            homework = check_response(response)
-            if len(homework) > 0:
-                hw_status = parse_status(homework[0])
-                if hw_status:
-                    send_message(bot, hw_status)
-            else:
-                logger.debug('В ответе отсутствует новый статус')
-            current_timestamp = response['current_date']
-            time.sleep(RETRY_TIME)
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            error_logging(message)
-            time.sleep(RETRY_TIME)
-        else:
-            if check_tokens() is not True:
-                break
+    if not check_tokens():
+        error_logging('Не пройдена проверка на наличие '
+                      'обязательных переменных окружения')
+    else:
+        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        current_timestamp = int(time.time())
+        while True:
+            try:
+                response = get_api_answer(current_timestamp)
+                homework = check_response(response)
+                if homework:
+                    hw_status = parse_status(homework[0])
+                    if hw_status:
+                        send_message(bot, hw_status)
+                else:
+                    logger.debug('В ответе отсутствует новый статус')
+                current_timestamp = response['current_date']
+            except Exception as error:
+                message = f'Сбой в работе программы: {error}'
+                error_logging(message)
+            finally:
+                time.sleep(settings.RETRY_TIME)
 
 
 if __name__ == '__main__':
